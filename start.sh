4976122b1a9b1696f87e240020f4bfb552f16dbd
abraxas678 <abraxas678@gmail.com>
2022-11-13 04:24:57 +0100

    1

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ clear
 echo "v0.5a"
 mkdir $HOME/tmp >/dev/null 2>/dev/null
 cd $HOME/tmp
+rm $HOME/tmp/start-b.sh* -f
 wget http://ionos:8027/start/start-b.sh >/dev/null 2>/dev/null
 chmod +x $HOME/tmp/*.sh
 echo
