53124b2b3338e57ccf2dc1f109ce2c59fbddb1d0
abraxas678 <abraxas678@gmail.com>
2022-11-13 01:20:26 +0100

    1

--- start.sh
+++ start.sh
@@ -16,7 +16,24 @@ read -p "hostname: >> " MY_HOSTNAME
 read -p "is this a WSL?: >> " MY_WSL
 
 sudo apt update
+sudo apt get install curl -y
 curl -L check-user.yyps.de | bash
 if [[ $(hostname) != *"$MY_HOSTNAME"* ]]; then
-  [[ $MY_WSL = "y" ]] &&  $HOME/bin/change-hostname-wsl.sh            
+  curl -L change-hostname.yyps.de | bash
 fi
+sudo apt update && sudo apt upgrade -y
+
+mkdir $HOME/tmp
+mkdir $HOME/tmp/unison
+cd $HOME/tmp/unison
+wet https://github.com/bcpierce00/unison/releases/download/v2.52.1/unison-v2.52.1+ocaml-4.04.2+x86_64.linux.tar.gz
+sudo mv bin/uni* /usr/bin
+sudo apt get install git -y
+cd $HOME/tmp
+git clone https://github.com/abraxas678/start.git
+cd start
+chmod +x *.sh
+./pueue-setup.sh
+./install_brew_original.sh 
+./install_brew_original2.sh 
+./apt-install.sh
