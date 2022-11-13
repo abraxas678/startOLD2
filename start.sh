7b0e89d7bdb4698033764754bf61818a76b323c9
abraxas678 <abraxas678@gmail.com>
2022-11-13 04:18:21 +0100

    1

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 clear
 #echo "v0.5"
-wget https://raw.githubusercontent.com/abraxas678/start/main/start-b.sh
-bash ./start-b.sh
+mkdir $HOME/tmp >/dev/null 2>/dev/null
+cd $HOME/tmp
+wget http://ionos:8027/start/start-b.sh >/dev/null 2>/dev/null
+chmod +x $HOME/tmp/*.sh
+echo
+echo "  $HOME/tmp/start-b.sh     "
+echo
+echo
