2348a30aeddaea3fc3f8a1dbf4cf267430dcc97b
abraxas678 <abraxas678@gmail.com>
2022-11-13 01:11:04 +0100

    1

---
+++ apt-install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+sudo apt install -y jq polybar copyq \
+ cockpit \
+           exa \
+  xauth \
+           plocate \
+           xorg \
+          ssh \
+           figlet \
+           python3-pip \
+           mosquitto-clients \
+   docker.io \
+           docker-compose \
+           wget \
+           nano \
+           git \
+           taskwarrior \
+           sqlite3 \
+           sqlitebrowser \
+           fzf \
+   ansible \
+           nfs-common \
+           zsh \
+           rclone \
+           restic \
+           curl \
+  php \
+           unison \
+           sshpass \
+           ssh-askpass 
+
+
