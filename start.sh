d50175134ad68470f8088fc11ec564cc0ce1832a
abraxas678 <abraxas678@gmail.com>
2022-11-14 17:21:59 +0100

    1

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "v0.5a"
 mkdir $HOME/tmp >/dev/null 2>/dev/null
 cd $HOME/tmp
 rm $HOME/tmp/start-b.sh* -f
-wget http://ionos:8027/start/start-b.sh >/dev/null 2>/dev/null
+wget http://aws:8027/start/start-b.sh >/dev/null 2>/dev/null
 chmod +x $HOME/tmp/*.sh
 echo
 echo "Manual steps:"
